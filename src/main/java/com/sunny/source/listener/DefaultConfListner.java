3c269336422ba6dd44b31308b236d9b55ded1fe8
junwei <junwei@nfs-qd.com>
2019-05-05 19:16:16 +0800

    提供默认的监听器，什么都不做

---
+++ src/main/java/com/sunny/source/listener/DefaultConfListner.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.sunny.source.listener;
+
+/**
+ * 默认的监听器<br>
+ * 什么都不做<br>
+ * 
+ * @author junehappylove
+ *
+ */
+public class DefaultConfListner implements ConfListner {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sunny.source.listener.ConfListner#doBefore()
+	 */
+	@Override
+	public void doBefore() {
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sunny.source.listener.ConfListner#doAfter()
+	 */
+	@Override
+	public void doAfter() {
+
+	}
+
+}
--- src/test/java/com/sunny/TestListener.java
+++ src/test/java/com/sunny/TestListener.java
@@ -2,14 +2,14 @@ package com.sunny;
 
 import com.sunny.source.listener.ConfListner;
 
-public class TestListener implements ConfListner{
-    @Override
-    public void doBefore() {
-        // TODO System.out.println("before");
-    }
+public class TestListener implements ConfListner {
+	@Override
+	public void doBefore() {
+		// TODO System.out.println("before");
+	}
 
-    @Override
-    public void doAfter() {
-    	// TODO System.out.println("after");
-    }
+	@Override
+	public void doAfter() {
+		// TODO System.out.println("after");
+	}
 }
